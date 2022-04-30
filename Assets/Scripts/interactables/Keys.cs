d973e67450d398e6219eaf7f9b2207574c4c8d54
kevponce4 <43938961+kevponce4@users.noreply.github.com>
2022-04-30 16:07:37 -0700

    super basic doors done

---
+++ Assets/Scripts/interactables/Keys.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keys : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerController>().keys_increase();
+            Destroy(this.gameObject);
+        }
+    }
+}
